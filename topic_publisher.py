181f6cf8e295f23d6a92eb8d533dd1a14e38c7c4
Reis25 <demetriosreis1@.com>
2019-06-03 20:31:18 -0300

    capitulo3
    Códigos de verificação de conceitos e definições;

---
+++ topic_publisher.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher('counter', Int32)
+rate = rospy.Rate(2)
+count = 2
+
+while not rospy.is_shutdown():
+ 	pub.publish(count)
+ 	count += 1
+ 	rate.sleep()
